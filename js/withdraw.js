9f98624ad75c9c731d489951eecfbf7466e935aa
raselsharif <raselsharif9@gmail.com>
2023-08-15 17:59:28 +0600

    added withdraw function with solved

--- js/withdraw.js
+++ js/withdraw.js
@@ -10,16 +10,22 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawFieldValue = parseFloat(withdrawFieldValueStr);
 
     const withdrawOutput = document.getElementById('withdraw-output');
+    const withdrawOutputStr = withdrawOutput.innerText;
+    const withdrawOutputTotal = parseFloat(withdrawOutputStr);
 
+    const withdrawOutputGrandTotal = withdrawOutputTotal + withdrawFieldValue;
 
     const balance = document.getElementById('balance')
     const balanceStr = balance.innerText;
     const totalBalance = parseFloat(balanceStr);
-    if (withdrawField.value > totalBalance) {
+    if (withdrawFieldValueStr > totalBalance) {
         alert("taka nai")
         return;
+    } else if (withdrawFieldValueStr <= 0) {
+        alert("Your can not put negative number or 0")
+        return;
     }
-    withdrawOutput.innerText = withdrawFieldValue;
+    withdrawOutput.innerText = withdrawOutputGrandTotal;
     const newTotalBalance = totalBalance - withdrawFieldValue;
 
     balance.innerText = newTotalBalance;
